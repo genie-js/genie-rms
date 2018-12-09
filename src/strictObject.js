92dc15a2b512ccf452fdf5163318be1d1dfddd5b
Renée Kooi <renee@kooi.me>
2018-12-09 15:41:57 +0100

    Implement toJSON on strictObject, fixes #2

--- src/strictObject.js
+++ src/strictObject.js
@@ -3,6 +3,7 @@ const whitelist = new Set(['inspect'])
 module.exports = typeof Proxy === 'function'
   ? o => new Proxy(o, {
     get (target, prop) {
+      if (prop === 'toJSON') return () => target
       const val = target[prop]
       if (typeof prop === 'symbol' || whitelist.has(prop)) return val
       if (val === undefined) throw new TypeError(`${prop} is undefined`)

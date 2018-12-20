70c10dcf51b5618061e3d4bdcc52ae08f9c20ed8
Renée Kooi <renee@kooi.me>
2018-12-20 12:02:59 +0100

    Read hardcoded DRS includes as strings.

--- src/hardcodedDrsIncludes.js
+++ src/hardcodedDrsIncludes.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-exports[54101] = fs.readFileSync(require.resolve('./std_resources.inc'))
-exports[54102] = fs.readFileSync(require.resolve('./land_and_water_resources.inc'))
-exports[54103] = fs.readFileSync(require.resolve('./land_resources.inc'))
+exports[54101] = fs.readFileSync(require.resolve('./std_resources.inc'), 'utf8')
+exports[54102] = fs.readFileSync(require.resolve('./land_and_water_resources.inc'), 'utf8')
+exports[54103] = fs.readFileSync(require.resolve('./land_resources.inc'), 'utf8')

a5e1c21279c322c4f7628e507b97dcd109be27f3
Renée Kooi <renee@kooi.me>
2018-12-20 11:47:46 +0100

    Add std_resources.inc.

--- src/hardcodedDrsIncludes.js
+++ src/hardcodedDrsIncludes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
 
+exports[54101] = fs.readFileSync(require.resolve('./std_resources.inc'))
 exports[54102] = fs.readFileSync(require.resolve('./land_and_water_resources.inc'))
 exports[54103] = fs.readFileSync(require.resolve('./land_resources.inc'))
